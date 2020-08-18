32141f78944d1aada112f33bcb91646a54565fd6
Fabricio Ishida <ishida.fabricio@gmail.com>
2020-08-18 08:44:49 -0300

    Desafio 1 bootcamp

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,79 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+
+  const repo = repositories.findIndex(repository => repository.id === id);
+
+
+  if(repo < 0){
+    return response.status(400).send();
+    
+  }
+
+  const repositoryNew = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories[repo] = repositoryNew;
+
+  return response.json(repositoryNew)
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repository = repositories.findIndex(repository => repository.id === id);
+
+  if(repository < 0){
+    return response.status(400).send()
+    
+  }
+  index = repositories.indexOf(repository);
+  repositories.splice(repository, 1);
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repository = repositories.findIndex(repository => repository.id === id);
+
+  if(repository < 0){
+    return response.status('400').send()
+
+  }
+
+  repositories[repository].likes = repositories[repository].likes + 1
+
+  return response.json(repositories[repository])
 });
 
 module.exports = app;
